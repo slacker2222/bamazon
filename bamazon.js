1103f09c3f25f14d450db55961cc2f0182fa6d8b
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-02-07 17:28:51 -0500

    still struggling

--- bamazon.js
+++ bamazon.js
@@ -7,76 +7,89 @@ var connection = mysql.createConnection({
     host: "localhost",
   
     // Your port; if not 3306
-    port: 3306,
+    port: 3300,
     user: "root",
     password: "22@Beeper22",
     database: "bamazon_db"
 });
 
-// connect to the mysql server and sql database
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    // run the start function after the connection is made to prompt the user
-    start();
+///Displays product information to user on load
+
+function displayData () {
+  console.log("Here are the products we have in stock.");
+  console.log("--------------------------------------");
+  connection.query ("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+          console.log("Item ID: " + res[i].item_id);
+          console.log("Name: " + res[i].product_name);
+          console.log("Department: " + res[i].department_name);
+          console.log("Price: " + res[i].price);
+          console.log("Num in stock: " + res[i].stock_quantity);
+          console.log("--------------------------------------");
+      }
+      askUser();
   });
 
-  // function which prompts the user for what action they should take
-function start() {
-    inquirer
-      .prompt({
-        name: "Shop",
-        type: "list",
-        message: "Would you like to [Shop]?",
-        choices: ["Shop", "EXIT"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.Bid === "Shop") {
-          shopBamazon();
-        }
-        else if(answer.postOrBid === "Exit") {
-          connection.end();
-        } else{
-          connection.end();
-        }
+}
+
+//function to gather user input on product to buy and quantity 
+
+function askUser() {
+
+  inquirer.prompt([
+      {
+          type: "input",
+          name: "desiredItemId",
+          message: "Please enter the Item ID of the product you would like to purchase:"
+
+      }, 
+      {
+          type: "input",
+          name: "desiredQuantity",
+          message: "How many units would you like to buy?"
+      }
+      
+  ]).then(function(user) {
+
+      connection.query("SELECT * FROM products", function(err,res) {
+          if (err) throw err;
+          
+          //condition to check whether or not there is enough product in stock
+          if (user.desiredQuantity <= res[user.desiredItemId - 1].stock_quantity) {
+              console.log("--------------------------------------");
+              console.log("You're order for " + res[user.desiredItemId -1].product_name + " has been placed!");
+              console.log("--------------------------------------");
+              console.log("Total Cost: $" + user.desiredQuantity * res[user.desiredItemId - 1].price);
+
+              //updating database with new product stock quantity as well as product sales (to be used in future exercise)
+              connection.query("UPDATE products SET ? WHERE ?",
+                  [
+                      {
+                          stock_quantity: res[user.desiredItemId - 1].stock_quantity - user.desiredQuantity,
+                          product_sales: res[user.desiredItemId - 1].product_sales + user.desiredQuantity * res[user.desiredItemId - 1].price
+                      },
+                      {
+                          item_id: user.desiredItemId
+                      }
+                  ],
+                  function(err) {
+                      if (err) throw err;
+                      console.log("--------------------------------------");
+                      console.log("For company: Products updated!");
+                      connection.end();
+                  }
+              )
+
+          } else {
+              console.log("Insufficient Quantity. Please enter a new amount or choose another product");
+
+                  //runs askUser function again to let user choose a different quantity or product altogether
+                  askUser();
+          }
       });
+  });
+}
 
-      function shopBamazon() {
-          console.log("searching for products...\n");
-          var query = connection.query(
-              "INSERT INTO products SET ?",
-              {
-                  product_name: "toaster",
-                  department_name: "kitchen",
-                  price: 10.00,
-                  stock_quantity: 30
-            },
-            function(err,res) {
-                console.log(res.affectedRows + " product inserted!\n");
-                updateProduct();
-            }
-          );
-
-          console.log(query.sql);
-      }
-      function updateProduct() {
-          console.log("Updating all products...\n")
-          var query = connection.query(
-              "UPDATE products SET ? Where?",
-              [
-                  { quaantity: 30
-                },
-                {
-                    product_name: "toaster"
-                }
-              ],
-        function(err, res) {
-            console.log(res.affectedRows + "products updated!\n");
-
-            deleteProduct();
-        }
-          );
-        console.log(query.sql);
-      }
-  }
+//initial execution of program
+displayData();
--- bamazon.sql
+++ bamazon.sql
@@ -9,36 +9,65 @@ USE bamazon_db;
 -- score INTEGER(10)
 -- );
 
--- CREATE TABLE favorite_songs (
--- Song VARCHAR(100) NOT NULL,
--- Artist VARCHAR(50),
--- Score INTEGER(10)
--- );
+
 
 CREATE TABLE products (
-id INTEGER NOT NULL AUTO_INCREMENT,
+item_id INTEGER NOT NULL AUTO_INCREMENT,
 product_name VARCHAR (100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
-price decimal (8,2) NOT NULL,
-stock_quantity integer(10),
-PRIMARY KEY (id)
+price INTEGER (12) NOT NULL,
+stock_quantity integer(10) NOT NULL,
+product_sales INTEGER (12) NOT NULL DEFAULT 0,
+PRIMARY KEY (item_id)
 
 );
 
-insert into products (id, product_name, department_name, price, stock_quantity)
-Values (1, "snowboard", "sports", 250.00, 1);
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("snowboard", "sports", 250, 20);
 
 
-insert into products (id, product_name, department_name, price, stock_quantity)
-Values (2, "baeball", "sports", 20.00, 2);
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("baeball", "sports", 20, 30);
 
 
-insert into products (id, product_name, department_name, price, stock_quantity)
-Values (3, "airfrier", "kitchen", 80.00, 6);
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("airfrier", "kitchen", 80, 15);
 
 
-insert into products (id, product_name, department_name, price, stock_quantity)
-Values (4, "crockpot", "kitchen", 15.00, 2);
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("crockpot", "kitchen", 15, 12);
 
-insert into products (id, product_name, department_name, price, stock_quantity)
-Values (5, "toaster", "kitchen", 10.00, 30);
+insert into products (product_name, department_name, price, stock_quantity)
+Values (
+    "toaster", "kitchen", 10, 30);
+
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("ski poles", "sports", 80, 50);
+
+
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("basketball", "sports", 40, 40);
+
+
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("cake plate", "kitchen", 15, 20);
+
+
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("coffee mug", "kitchen", 16, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+Values ("coffee maker", "kitchen", 175, 60);
+
+CREATE TABLE departments (
+	department_id INTEGER (11) AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    over_head_costs INTEGER(11) NOT NULL,
+    PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("kitchen", 9000);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("sports", 30000);
